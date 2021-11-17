97f34ac67a9e77aaf3501a882a80db03b08b6ff2
jvcataquiz <jvcataquiz@gmail.com>
2021-11-17 23:54:34 +0800

    Done creating buttons (1-9) and the temporary design

---
+++ tictactoe/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace tictactoe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOne = new System.Windows.Forms.Button();
+            this.buttonTwo = new System.Windows.Forms.Button();
+            this.buttonThree = new System.Windows.Forms.Button();
+            this.buttonSix = new System.Windows.Forms.Button();
+            this.buttonFive = new System.Windows.Forms.Button();
+            this.buttonFour = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonOne
+            // 
+            this.buttonOne.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(185)))), ((int)(((byte)(151)))));
+            this.buttonOne.FlatAppearance.BorderColor = System.Drawing.Color.Tomato;
+            this.buttonOne.FlatAppearance.BorderSize = 0;
+            this.buttonOne.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F);
+            this.buttonOne.Location = new System.Drawing.Point(30, 143);
+            this.buttonOne.Name = "buttonOne";
+            this.buttonOne.Padding = new System.Windows.Forms.Padding(8, 3, 0, 0);
+            this.buttonOne.Size = new System.Drawing.Size(100, 100);
+            this.buttonOne.TabIndex = 0;
+            this.buttonOne.TabStop = false;
+            this.buttonOne.UseVisualStyleBackColor = false;
+            this.buttonOne.Click += new System.EventHandler(this.bntone_Click);
+            // 
+            // buttonTwo
+            // 
+            this.buttonTwo.BackColor = System.Drawing.Color.Salmon;
+            this.buttonTwo.FlatAppearance.BorderSize = 0;
+            this.buttonTwo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F);
+            this.buttonTwo.Location = new System.Drawing.Point(156, 141);
+            this.buttonTwo.Name = "buttonTwo";
+            this.buttonTwo.Size = new System.Drawing.Size(100, 100);
+            this.buttonTwo.TabIndex = 1;
+            this.buttonTwo.TabStop = false;
+            this.buttonTwo.UseVisualStyleBackColor = false;
+            this.buttonTwo.Click += new System.EventHandler(this.buttonTwo_Click);
+            // 
+            // buttonThree
+            // 
+            this.buttonThree.BackColor = System.Drawing.Color.Salmon;
+            this.buttonThree.FlatAppearance.BorderSize = 0;
+            this.buttonThree.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonThree.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F);
+            this.buttonThree.Location = new System.Drawing.Point(284, 141);
+            this.buttonThree.Name = "buttonThree";
+            this.buttonThree.Size = new System.Drawing.Size(100, 100);
+            this.buttonThree.TabIndex = 2;
+            this.buttonThree.TabStop = false;
+            this.buttonThree.UseVisualStyleBackColor = false;
+            this.buttonThree.Click += new System.EventHandler(this.buttonThree_Click);
+            // 
+            // buttonSix
+            // 
+            this.buttonSix.BackColor = System.Drawing.Color.Salmon;
+            this.buttonSix.FlatAppearance.BorderSize = 0;
+            this.buttonSix.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSix.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F);
+            this.buttonSix.Location = new System.Drawing.Point(284, 253);
+            this.buttonSix.Name = "buttonSix";
+            this.buttonSix.Size = new System.Drawing.Size(100, 100);
+            this.buttonSix.TabIndex = 5;
+            this.buttonSix.TabStop = false;
+            this.buttonSix.UseVisualStyleBackColor = false;
+            // 
+            // buttonFive
+            // 
+            this.buttonFive.BackColor = System.Drawing.Color.Salmon;
+            this.buttonFive.FlatAppearance.BorderSize = 0;
+            this.buttonFive.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonFive.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F);
+            this.buttonFive.Location = new System.Drawing.Point(156, 253);
+            this.buttonFive.Name = "buttonFive";
+            this.buttonFive.Size = new System.Drawing.Size(100, 100);
+            this.buttonFive.TabIndex = 4;
+            this.buttonFive.TabStop = false;
+            this.buttonFive.UseVisualStyleBackColor = false;
+            // 
+            // buttonFour
+            // 
+            this.buttonFour.BackColor = System.Drawing.Color.Salmon;
+            this.buttonFour.FlatAppearance.BorderSize = 0;
+            this.buttonFour.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonFour.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F);
+            this.buttonFour.Location = new System.Drawing.Point(30, 253);
+            this.buttonFour.Name = "buttonFour";
+            this.buttonFour.Size = new System.Drawing.Size(100, 100);
+            this.buttonFour.TabIndex = 3;
+            this.buttonFour.TabStop = false;
+            this.buttonFour.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Salmon;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F);
+            this.button1.Location = new System.Drawing.Point(284, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 8;
+            this.button1.TabStop = false;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Salmon;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F);
+            this.button2.Location = new System.Drawing.Point(156, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 7;
+            this.button2.TabStop = false;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Salmon;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F);
+            this.button3.Location = new System.Drawing.Point(30, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 6;
+            this.button3.TabStop = false;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(124, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tic Tac Toe";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.GhostWhite;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.buttonOne);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.buttonFour);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.buttonTwo);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.buttonThree);
+            this.groupBox1.Controls.Add(this.buttonSix);
+            this.groupBox1.Controls.Add(this.buttonFive);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(0);
+            this.groupBox1.Size = new System.Drawing.Size(413, 510);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(444, 534);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOne;
+        private System.Windows.Forms.Button buttonTwo;
+        private System.Windows.Forms.Button buttonThree;
+        private System.Windows.Forms.Button buttonSix;
+        private System.Windows.Forms.Button buttonFive;
+        private System.Windows.Forms.Button buttonFour;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ tictactoe/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictactoe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bntone_Click(object sender, EventArgs e)
+        {
+
+            this.buttonOne.Text = "X";
+            this.buttonOne.Enabled = false;
+        }
+
+        private void buttonTwo_Click(object sender, EventArgs e)
+        {
+            this.buttonTwo.Text = "X";
+            this.buttonThree.Enabled = false;
+        }
+
+        private void buttonThree_Click(object sender, EventArgs e)
+        {
+            this.buttonThree.Text = "X";
+            this.buttonThree.Enabled = false;
+        }
+
+      
+    }
+}
